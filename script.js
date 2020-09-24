6690030a8f9cd3a4b97c4eb6f800e51c044022ec
Jane Lane <loan.does@gmail.com>
2020-09-24 17:26:31 +0200

    implémentation carrousel jquery

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $("body").on("click", ".flechedroite i", function() {
+      changeImage("droite");
+  });
+
+  $("body").on("click", ".flechegauche i", function() {
+      changeImage("gauche");
+  });
+
+});
+
+
+let state = 0;
+
+function changeImage(direction) {
+  if (direction === "droite") {
+    state = (state + 1)% 3;
+  } else if (direction === "gauche") {
+    state = (state - 1)%3;
+  }
+
+  if (state === 0) {
+    $("#carrousel-image").attr("src", "images/florian.png");
+    $(".textetemoignage .text").html('"L\'absence totale de compétition m\'a permis de m\'ouvrir à l\'aspect créatif du code"');
+  } else if (state === 1 || state === -1) {
+    $("#carrousel-image").attr("src", "images/lydia.png");
+    $(".textetemoignage .text").html('"J\'adore la moto"');
+  } else if (state === 2 || state === -2) {
+    $("#carrousel-image").attr("src", "images/marine.png");
+    $(".textetemoignage .text").html('"Je travaille chez LinkedIn"');
+  }
+
+}
