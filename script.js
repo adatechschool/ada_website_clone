791674f32e4022efec202160bba8a9c3c288c4d8
del <w.bounkeua@gmail.com>
2020-09-24 17:35:00 +0200

    parce qu'on peut ecrit moins de code

--- script.js
+++ script.js
@@ -16,18 +16,17 @@ function changeImage(direction) {
   if (direction === "droite") {
     state = (state + 1)% 3;
   } else if (direction === "gauche") {
-    state = (state - 1)%3;
+    state = (state + 2)%3;
   }
 
   if (state === 0) {
     $("#carrousel-image").attr("src", "images/florian.png");
     $(".textetemoignage .text").html('"L\'absence totale de compétition m\'a permis de m\'ouvrir à l\'aspect créatif du code"');
-  } else if (state === 1 || state === -1) {
+  } else if (state === 1) {
     $("#carrousel-image").attr("src", "images/lydia.png");
     $(".textetemoignage .text").html('"J\'adore la moto"');
-  } else if (state === 2 || state === -2) {
+  } else if (state === 2) {
     $("#carrousel-image").attr("src", "images/marine.png");
     $(".textetemoignage .text").html('"Je travaille chez LinkedIn"');
   }
-
 }
